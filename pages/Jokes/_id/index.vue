bf166a4f51427b7a7b72db72b05d7d1211f3cd9d
Mark Packham <markpackham1@gmail.com>
2019-05-04 21:24:01 +0100

    target specific jokes by id https://youtu.be/ltzlhAxJr74?t=2076

--- pages/Jokes/_id/index.vue
+++ pages/Jokes/_id/index.vue
@@ -1,21 +1,19 @@
 <template>
-    <div>
-        <Joke v-for="joke in jokes" :key="joke.id"
-        :id="joke.id" :joke="joke.joke"
-        /></div>
+  <div>
+    <nuxt-link to="/jokes">Back To Jokes</nuxt-link>
+    <h2>{{ joke }}</h2>
+    <hr>
+    <small>Joke ID: {{ $route.params.id }}</small>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
-import Joke from '../../components/Joke';
 
 export default {
-    components:{
-    Joke
-    },
   data() {
     return {
-      jokes: []
+      joke: {}
     };
   },
   async created() {
@@ -26,16 +24,19 @@ export default {
     };
 
     try {
-      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      const res = await axios.get(
+        `https://icanhazdadjoke.com/j/${this.$route.params.id}`,
+        config
+      );
 
-      this.jokes = res.data.results;
+      this.joke = res.data.joke;
     } catch (err) {
       console.log(err);
     }
   },
   head() {
     return {
-      title: "Dad jokes",
+      title: this.joke,
       meta: [
         {
           hid: "description",
