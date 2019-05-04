7153dc9395635dca024bdc28cfecaef08e4dae8a
Mark Packham <markpackham1@gmail.com>
2019-05-04 22:22:11 +0100

    finish off app with search functionality

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,18 +1,20 @@
 <template>
-    <div>
-        <Joke v-for="joke in jokes" :key="joke.id"
-        :id="joke.id" :joke="joke.joke"
-        /></div>
+  <div>
+    <SearchJokes v-on:search-text="searchText"/>
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke"/>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
-import Joke from '../../components/Joke';
+import Joke from "../../components/Joke";
+import SearchJokes from "../../components/SearchJokes";
 
 export default {
-    components:{
-    Joke
-    },
+  components: {
+    Joke,
+    SearchJokes
+  },
   data() {
     return {
       jokes: []
@@ -33,9 +35,29 @@ export default {
       console.log(err);
     }
   },
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
+
+      try {
+        const res = await axios.get(
+          `https://icanhazdadjoke.com/search?term=${text}`,
+          config
+        );
+
+        this.jokes = res.data.results;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
   head() {
     return {
-      title: "Dad jokes",
+      title: "Dad Jokes",
       meta: [
         {
           hid: "description",
